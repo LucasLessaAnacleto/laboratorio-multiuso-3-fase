aa4e7711144db83737a58a218d5c97b02be5e290
Seu Nome <seuemail@example.com>
2025-11-14 21:03:58 -0300

    start project Spring Boot para backend

---
+++ backend/src/test/java/com/extensao/senac/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extensao.senac.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
